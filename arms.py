7d7525ffe0890aba306d1cf8e3e34d8c8c81559d
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-01-21 05:46:33 +0000

    arms.py

--- arms.py
+++ arms.py
@@ -1,1 +1,14 @@
 
+upper=int(input("upper case"))
+lower=int(input("lower case"))
+for i in range(lower, upper + 1):
+   order = len(str(i))
+   sum = 0
+   temp = i
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** order
+       temp //= 10
+
+   if i == sum:
+       print(i)
