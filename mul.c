2b31d7ede813e69f4252e42af2712f27c304f5b6
bass18598 <35221213+bass18598@users.noreply.github.com>
2018-01-21 06:46:48 +0000

    mul.c

---
+++ mul.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+        longint sum=0,i,n;
+        clrscr();
+        printf("\n The Value of N:  ");
+        scanf("%ld",&n);
+        for(j=1;j<=n;j++)
+        {
+            printf("\n 5 * %ld = %ld",i,5*i);
+        }
+        
+        getch();
